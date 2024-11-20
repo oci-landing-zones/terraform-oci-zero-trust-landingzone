9107bc6a5a0738493fbba78bd2d27e0baa527dff
Yupei Yang <yupei.yang@oracle.com>
2024-11-20 12:52:54 -0800

    update github urls

--- main.tf
+++ main.tf
@@ -14,40 +14,46 @@
 # -------------------------------------------------------------------------------------
 
 module "zero_trust_landing_zone" {
-  source = "../oci-core-landing-zone" #"github.com/oci-landing-zones/terraform-oci-core-landingzone?ref=v1.1.0"
+  source = "github.com/oci-landing-zones/terraform-oci-core-landingzone?ref=v1.1.0"
   # ------------------------------------------------------
   # ----- Environment
   # ------------------------------------------------------
-  tenancy_ocid         = "ocid1.tenancy.oc1..aaaaaaaahbsqqoq6hngruus5z4e3zclij32obawvlsxsrz4culbvq5k5p2ia"               # Replace with your tenancy OCID.
-  user_ocid            = "ocid1.user.oc1..aaaaaaaacm7yvg4f5tvm4jdf4qsnuo47fsywie7ldniit4nkfnt64hbrdg6q"                  # Replace with your user OCID.
-  fingerprint          = "d4:ce:c1:ad:79:c3:6a:fd:f5:57:46:0b:e5:b4:c3:26"                                               # Replace with user fingerprint.
-  private_key_path     = "/Users/yupeyang/.oci/oci_api_key.pem"                                                          # Replace with user private key local path.
-  private_key_password = ""                                                                                              # Replace with private key password, if any.
-  region               = "us-phoenix-1" # Replace with region name.
-  service_label        = "ztemp"     # Prefix prepended to deployed resource names.
+  tenancy_ocid         = "" # Replace with your tenancy OCID.
+  user_ocid            = "" # Replace with your user OCID.
+  fingerprint          = "" # Replace with user fingerprint.
+  private_key_path     = "" # Replace with user private key local path.
+  private_key_password = "" # Replace with private key password, if any.
+  region               = "" # Replace with region name.
+  service_label        = "" # Prefix prepended to deployed resource names.
+
+  # ------------------------------------------------------
+  # ----- General
+  # ------------------------------------------------------
+  cis_level = 2 # Options: 1, 2
 
   # ------------------------------------------------------
   # ----- Hub Deployment Option
   # ------------------------------------------------------
-  hub_deployment_option     = "VCN or on-premises connectivity routing through DMZ VCN with Network Virtual Appliance (DRG and DMZ VCN will be created)"
+  hub_deployment_option = "VCN or on-premises connectivity routing through DMZ VCN with Network Virtual Appliance (DRG and DMZ VCN will be created)"
 
   # ------------------------------------------------------
   # ----- Network Appliance Option
   # ------------------------------------------------------
-  hub_vcn_deploy_net_appliance_option = "Fortinet FortiGate Firewall"
-  net_fortigate_version               = "7.2.9_(_X64_)"
-  net_appliance_flex_shape_memory     = 56
-  net_appliance_flex_shape_cpu        = 4
-  net_appliance_boot_volume_size      = 60
-  net_appliance_public_rsa_key        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMffzPTbnVZdf8HWXnCzdWYrCUdYRNPEaqhEOIF3CS7t <yupei.yang@oracle.com>"#Enter the Public RSA Key
-  net_appliance_shape                 = "VM.Standard.E4.Flex"
+  hub_vcn_deploy_net_appliance_option = "Fortinet FortiGate Firewall" # Option: "Don't deploy any network appliance at this time", "Palo Alto Networks VM-Series Firewall", "Fortinet FortiGate Firewall"
+  net_fortigate_version               = "7.2.9_(_X64_)"               # Option: "7.4.4_(_X64_)", "7.2.9_(_X64_)"
+  # net_palo_alto_version               = "" # Option: "11.1.3", "11.1.2-h3"
+  net_appliance_flex_shape_memory = 56
+  net_appliance_flex_shape_cpu    = 4
+  net_appliance_boot_volume_size  = 60
+  net_appliance_public_rsa_key    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMffzPTbnVZdf8HWXnCzdWYrCUdYRNPEaqhEOIF3CS7t <yupei.yang@oracle.com>" #Enter the Public RSA Key
+  net_appliance_shape             = "VM.Standard.E4.Flex"
 
-# ------------------------------------------------------
+  # ------------------------------------------------------
   # ----- Networking
   # ------------------------------------------------------
-  define_net  = true # enables network resources provisioning
-  enable_zpr  = true # enables Zero Trust Packet Routing at the tenancy level
-  add_tt_vcn1 = true # This deploys one three-tier VCN with default settings, like default name, CIDR, DNS name, subnet names, subnet CIDRs, subnet DNS names.
+  define_net            = true # enables network resources provisioning
+  enable_zpr            = true # enables Zero Trust Packet Routing at the tenancy level
+  add_tt_vcn1           = true # This deploys one three-tier VCN with default settings, like default name, CIDR, DNS name, subnet names, subnet CIDRs, subnet DNS names.
   tt_vcn1_attach_to_drg = true # attach three-tier VCN to the DRG
 
   # ------------------------------------------------------
@@ -59,8 +65,8 @@ module "zero_trust_landing_zone" {
   # ------------------------------------------------------
   # ----- Logging
   # ------------------------------------------------------
-  enable_service_connector      = true # Enables service connector for logging consolidation.
-  activate_service_connector    = true # Activates service connector.
+  enable_service_connector      = true        # Enables service connector for logging consolidation.
+  activate_service_connector    = true        # Activates service connector.
   service_connector_target_kind = "streaming" # Sends collected logs to an OCI stream.
 
 
@@ -68,8 +74,8 @@ module "zero_trust_landing_zone" {
   # ----- Security
   # ------------------------------------------------------
   enable_cloud_guard    = false # Set to false if Cloud Guard has already been enabled.
-  enable_security_zones = true # Deploys a security zone for this deployment in the enclosing compartment.
-  vss_create            = true # Enables Vulnerability Scanning Service for Compute instances.
+  enable_security_zones = true  # Deploys a security zone for this deployment in the enclosing compartment.
+  vss_create            = true  # Enables Vulnerability Scanning Service for Compute instances.
 
   # ------------------------------------------------------
   # ----- Governance
